4b6bcd53e2cf78ab844ddbc799496e4e16e0c031
luis teran <luis.teran@adevinta.com>
2019-12-04 12:53:30 -0300

    [feat/add_travis] Added makefile and scripts

---
+++ api-leads/scripts/vars.mk
@@ -0,0 +1,8 @@
+
+
+export APPNAME=api-leads
+export VERSION=0.0.1
+
+
+export DOCKER_IMAGE=${DOCKER_REGISTRY}/yapo/${APPNAME}
+export DOCKER_IMAGE_COMPOSE=${DOCKER_REGISTRY}/yapo/${APPNAME}:${GIT_BRANCH}
