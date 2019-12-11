dda17d91905a0535d1b465b6e9c14f6f0a9793bb
luis teran <luis.teran@adevinta.com>
2019-12-11 11:05:07 -0300

    [feat/enable_pep8] Enabled pep8 in pre-commit hook
    Also:
    	rewrite how secrets and passwords are accessed

--- api-leads/scripts/vars.mk
+++ api-leads/scripts/vars.mk
@@ -1,7 +1,9 @@
 # Application variables
-export APPNAME=api-leads
+export APPNAME=data-api-leads
 export VERSION=0.0.1
 
 # Docker variables
 export DOCKER_IMAGE=${DOCKER_REGISTRY}/yapo/${APPNAME}
 export DOCKER_IMAGE_COMPOSE=${DOCKER_REGISTRY}/yapo/${APPNAME}:${GIT_BRANCH}
+export APP_XITI_SECRET=xiti-secret
+export APP_DB_SECRET=db-secret
