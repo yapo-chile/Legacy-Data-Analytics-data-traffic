69a3387e8dd86ce356b8be20b61a0d1777f30edb
luis teran <luis.teran@adevinta.com>
2019-12-13 11:28:30 -0300

    [feat/rundeck-deploy] Added job definition and deploy

--- api-leads/scripts/rundeck-deploy.sh
+++ api-leads/scripts/rundeck-deploy.sh
@@ -5,10 +5,14 @@ DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 . "$DIR/colors.sh"
 
+base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 echoHeader "Deploying job definition to rundeck"
 
 set +e
 
-curl -X GET \
-  http://3.94.225.3:4440/api/14/project/Test/jobs \
+curl -X POST \
+  http://3.94.225.3:4440/api/14/project/data_jobs/jobs \
+  -H "Content-Type: application/yaml" \
   -H "X-Rundeck-Auth-Token: ${RUNDECK_TOKEN}" \
+  -d "@${base_dir}/../deploy/api_leads.yaml"
