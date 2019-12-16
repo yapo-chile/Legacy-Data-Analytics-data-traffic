8dbb3f326749601ffc40faf3f56fbacefbd49b07
luis teran <luis.teran@adevinta.com>
2019-12-16 17:28:53 -0300

    [feat/rundeck-deploy] change deploy script

--- api-leads/scripts/rundeck-deploy.sh
+++ api-leads/scripts/rundeck-deploy.sh
@@ -11,4 +11,4 @@ echoHeader "Deploying job definition to rundeck"
 
 set +e
 
-cat ${base_dir}/../deploy/api_leads.yaml | xargs -I{} curl -X POST   'http://3.94.225.3:4440/api/14/project/data_jobs/jobs/import?fileformat=yaml&dupeOption=create&uuidOption=preserve'   -H 'Content-Type: application/yaml'   -H 'X-Rundeck-Auth-Token: 0gKxHfpkkL8q94XgwXWdKZDzyadIuM9F' --data-binary '{}'
+cat ${base_dir}/../deploy/api_leads.yaml | curl -X POST 'http://3.94.225.3:4440/api/14/project/data_jobs/import?fileformat=yaml&dupeOption=update&uuidOption=preserve' -H 'Content-Type: application/yaml' -H 'X-Rundeck-Auth-Token: '${RUNDECK_TOKEN}'' --data-binary '@-'
