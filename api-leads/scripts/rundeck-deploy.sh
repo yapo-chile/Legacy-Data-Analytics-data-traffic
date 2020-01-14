0d8b99d47b25e7154cea7d3c2c2e98131d0490b3
luis teran <luis.teran@adevinta.com>
2020-01-14 11:32:29 -0300

    [feat/rundeck-deploy] Added rundeck publish condition

--- api-leads/scripts/rundeck-deploy.sh
+++ api-leads/scripts/rundeck-deploy.sh
@@ -11,4 +11,6 @@ echoHeader "Deploying job definition to rundeck"
 
 set +e
 
-cat ${base_dir}/../deploy/api_leads.yaml | curl -X POST 'http://3.94.225.3:4440/api/14/project/data_jobs/jobs/import?fileformat=yaml&dupeOption=update&uuidOption=preserve' -H 'Content-Type: application/yaml' -H 'X-Rundeck-Auth-Token: '${RUNDECK_TOKEN}'' --data-binary '@-'
+if [[ "${GIT_BRANCH_LOWERCASE}" == "master" ]]; then
+    cat ${base_dir}/../deploy/api_leads.yaml | curl -X POST 'http://3.94.225.3:4440/api/14/project/data_jobs/jobs/import?fileformat=yaml&dupeOption=update&uuidOption=preserve' -H 'Content-Type: application/yaml' -H 'X-Rundeck-Auth-Token: '${RUNDECK_TOKEN}'' --data-binary '@-'
+fi
