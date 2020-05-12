c80bbf42ff769e7c5e1f9e7eb6e994a9b4111a36
aaron-dominguez <aaron.dominguez@adevinta.com>
2020-05-12 15:52:04 -0400

    [ci/rundeck-retryable]: adding retry config to traffic pipelines

--- api-leads/scripts/rundeck-deploy.sh
+++ api-leads/scripts/rundeck-deploy.sh
@@ -10,7 +10,8 @@ base_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echoHeader "Deploying job definition to rundeck"
 
 set +e
-
+echo "GIT BRANCH: ${GIT_BRANCH}"
 if [[ "${GIT_BRANCH}" == "master" ]]; then
-    cat ${base_dir}/../deploy/api_leads.yaml | curl -X POST 'http://3.94.225.3:4440/api/14/project/data_jobs/jobs/import?fileformat=yaml&dupeOption=update&uuidOption=preserve' -H 'Content-Type: application/yaml' -H 'X-Rundeck-Auth-Token: '${RUNDECK_TOKEN}'' --data-binary '@-'
+    echo "POST to ${RUNDECK_ENDPOINT}"
+    cat ${base_dir}/../deploy/api_leads.yaml | curl -X POST 'http://'${RUNDECK_ENDPOINT}'/api/14/project/data_jobs/jobs/import?fileformat=yaml&dupeOption=update&uuidOption=preserve' -H 'Content-Type: application/yaml' -H 'X-Rundeck-Auth-Token: '${RUNDECK_TOKEN}'' --data-binary '@-'
 fi
